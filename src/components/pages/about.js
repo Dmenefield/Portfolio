351c4d31283d696c0cb91d673b12dde75a341b4c
Dmenefield <dmenefieldjr@gmail.com>
2021-06-29 12:49:22 -0700

    prototype 1

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -9,9 +9,11 @@ export default function() {
         paddingRight: '30px',
         fontSize: '1.5rem',
         fontFamily: 'inherit'
-      }} >  Hello!  I am a full stack web developer born in Woodland hills, California with about 3 years of coding experience. 
-Certified in:  Javascript, ReactJS, Python 3, HTML5, CSS, JSON, Git, SQL Databases, SCSS/SASS, Flexbox, & CSS Grid. Growing up, videogames and computers made me very passionate about technology and what drew me to programming was the endless possibilities that a career in coding provides. Coding allows me to express my passion for storytelling by bringing my clients story to life through web design. Ultimately my goal is to create beautiful things that serve beautiful purposes.
-
+      }} > 
+      <div className="about-wrapper">
+      <p className="about-text"> Hello!  I am a full stack web developer born in Woodland hills, California with about 3 years of coding experience. 
+Certified in:  Javascript, ReactJS, Python 3, HTML5, CSS, JSON, Git, SQL Databases, SCSS/SASS, Flexbox, & CSS Grid. Growing up, videogames and computers made me very passionate about technology and what drew me to programming was the endless possibilities that a career in coding provides. Coding allows me to express my passion for storytelling by bringing my clients story to life through web design. Ultimately my goal is to create beautiful things that serve beautiful purposes.</p>
+      </div>
      </div>
         <div 
         className="right-column"
