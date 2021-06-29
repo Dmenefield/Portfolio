d916c01c234f5d9e5325580e01c1ee4defa361fa
Dmenefield <dmenefieldjr@gmail.com>
2021-06-29 13:50:19 -0700

    final commit

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -11,7 +11,7 @@ export default function() {
         fontFamily: 'inherit'
       }} > 
       <div className="about-wrapper">
-      <p className="abo"> Hello!  I am a full stack web developer born in Woodland hills, California with about 3 years of coding experience. 
+      <p className="about-text"> Hello!  I am a full stack web developer born in Woodland hills, California with about 3 years of coding experience. 
 Certified in:  Javascript, ReactJS, Python 3, HTML5, CSS, JSON, Git, SQL Databases, SCSS/SASS, Flexbox, & CSS Grid. Growing up, videogames and computers made me very passionate about technology and what drew me to programming was the endless possibilities that a career in coding provides. Coding allows me to express my passion for storytelling by bringing my clients story to life through web design. Ultimately my goal is to create beautiful things that serve beautiful purposes.</p>
       </div>
      </div>
